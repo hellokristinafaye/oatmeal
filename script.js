fa5d5ccffd3c1510373c7dfa860f7ca7ca41ca6a
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-05 12:43:32 -0700

    for loop added, successfully counts to user input

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 console.log("the power of the force")
 
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+    console.log(i);
+}
