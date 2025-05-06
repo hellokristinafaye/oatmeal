4a43bb70520ce855b8ccd9a2cc729cd3eeae9de4
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-06 12:26:22 -0700

    first condition prints properly

--- soggy.js
+++ soggy.js
@@ -1,5 +1,9 @@
 let answer = parseInt(prompt("Enter the number to which you want this app to fizzbuzz to."))
 
-for (let i = 0; i <= answer; i++) {
-    console.log(i);
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0) {
+        console.log("fizz")
+    } else {
+        console.log(i);
+    }
 }
