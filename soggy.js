2b4d0f6cd2a4b865f9430c2c58b22f14a41a5931
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-06 12:27:21 -0700

    condition for 5 works

--- soggy.js
+++ soggy.js
@@ -2,8 +2,12 @@ let answer = parseInt(prompt("Enter the number to which you want this app to fiz
 
 for (let i = 1; i <= answer; i++) {
     if (i % 3 === 0) {
-        console.log("fizz")
-    } else {
+        console.log("fizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    }
+    
+    else {
         console.log(i);
     }
 }
