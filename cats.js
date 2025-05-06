351b9b44bc1566c3dc06ce5c67d9a1cdbc4340f9
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-06 15:42:16 -0700

    array v string methods

--- cats.js
+++ cats.js
@@ -1,5 +1,12 @@
+// const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+// for (const cat of cats) {
+//     console.log(cat);
+// }
+
 const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
 
-for (const cat of cats) {
-    console.log(cat);
-}
+const upperCats = cats.map();
+
+console.log(upperCats);
+// oh I see b/c toUpperCase is a string method, and wouldn't work on an array.  So you gotta make it its own function to make it work on an array. 
