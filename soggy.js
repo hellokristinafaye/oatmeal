177c12d0816f5454496d5290f4378519e6d19283
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-06 12:28:42 -0700

    FizzBuzz condition works!

--- soggy.js
+++ soggy.js
@@ -1,12 +1,15 @@
 let answer = parseInt(prompt("Enter the number to which you want this app to fizzbuzz to."))
 
 for (let i = 1; i <= answer; i++) {
-    if (i % 3 === 0) {
-        console.log("fizz");
-    } else if (i % 5 === 0) {
-        console.log("buzz");
+    if (i%3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
     }
     
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
     else {
         console.log(i);
     }
